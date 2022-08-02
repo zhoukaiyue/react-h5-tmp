0b8acac06a88e76d0dd10650cdc9a29194b3d3f6
zhoukaiyue <1301524439@qq.com>
2022-08-02 12:16:19 +0800

    fix：全局UI优化

--- src/config/router/index.js
+++ src/config/router/index.js
@@ -4,10 +4,15 @@
  * @Author: zhoukai
  * @Date: 2022-07-29 11:03:13
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-07-31 20:17:11
+ * @LastEditTime: 2022-08-02 10:22:33
  */
 import index from "@/views/index"
 const routes = [
+    {
+        path: "/",
+        component: index,
+        exact: true,
+    },
     {
         path: "/index",
         component: index,
