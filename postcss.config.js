666d8b523328d90e1e583294c8f9f2ec4bbe1669
zhoukaiyue <1301524439@qq.com>
2022-08-02 16:11:45 +0800

    更新postcss配置文件

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,18 @@
+/*
+ * @Descripttion: postcss 配置文件
+ * @version:
+ * @Author: zhoukai
+ * @Date: 2022-07-29 10:05:44
+ * @LastEditors: zhoukai
+ * @LastEditTime: 2022-08-02 16:10:42
+ */
 // postcss.config.js
 module.exports = {
     plugins: {
         // postcss-pxtorem 插件的版本需要 >= 5.0.0
         "postcss-pxtorem": {
             rootValue({ file }) {
-                return file.indexOf("vant") !== -1 ? 37.5 : 75
+                return file.indexOf("antd-mobile") !== -1 ? 37.5 : 75
             },
             propList: ["*"],
         },
