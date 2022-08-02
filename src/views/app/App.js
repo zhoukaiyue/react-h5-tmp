488161df8a6fb3b533dc5b2c2dc9d41fd47e3607
zhoukaiyue <1301524439@qq.com>
2022-08-02 21:56:51 +0800

    fix：优化

--- src/views/app/App.js
+++ src/views/app/App.js
@@ -2,7 +2,7 @@
  * @Author: zhoukaiyue 1301524439@qq.com
  * @Date: 2022-07-28 09:54:02
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-02 14:56:36
+ * @LastEditTime: 2022-08-02 21:54:03
  * @FilePath: \react-h5\src\App.js
  * @Description: 顶级 router-view
  */
@@ -11,10 +11,12 @@ import React from "react"
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import routers from "@/packages/router"
+// router基路径
+const ROUTER_BASE = process.env.REACT_APP_ROUTER_BASE
 
 const App = () => {
     return (
-        <Router>
+        <Router basename={ROUTER_BASE}>
             <Routes>
                 {routers.map((item, index) => {
                     return (
