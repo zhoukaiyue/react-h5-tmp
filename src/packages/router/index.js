776bf6b3284f4d7541fb2a332a9e0aa6a10d61c8
zhoukaiyue <1301524439@qq.com>
2022-08-05 16:54:59 +0800

    fix: 添加限制

--- src/packages/router/index.js
+++ src/packages/router/index.js
@@ -4,7 +4,7 @@
  * @Author: zhoukai
  * @Date: 2022-07-29 16:29:36
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-04 10:00:23
+ * @LastEditTime: 2022-08-05 16:54:08
  */
 
 import router404 from './router404.js';
@@ -20,6 +20,13 @@ const modules = modulesFiles.keys().reduce((modules, modulePath) => {
 const routers = [];
 
 for (const key in modules) {
+    // if (key === 'dev' && ['development'].includes(process.env.NODE_ENV)) {
+    //     routers.push(...modules[key]);
+    // }
+    // if (key !== 'dev') {
+    //     routers.push(...modules[key]);
+    // }
+
     routers.push(...modules[key]);
 }
 // 404页面
