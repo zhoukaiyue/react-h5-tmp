95ca4690167b1a29291fdd0decc5c5e51a8e087b
zhoukaiyue <1301524439@qq.com>
2022-08-05 17:41:59 +0800

    fix: 组件位置更换

--- src/views/app/App.js
+++ src/views/app/App.js
@@ -2,7 +2,7 @@
  * @Author: zhoukaiyue 1301524439@qq.com
  * @Date: 2022-07-28 09:54:02
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-04 14:54:55
+ * @LastEditTime: 2022-08-05 17:41:17
  * @FilePath: \react-h5\src\App.js
  * @Description: 顶级 router-view
  */
@@ -11,7 +11,7 @@ import React, { Suspense } from 'react';
 
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import routers from '@/packages/router';
-import routerLoading from '../routerLoading';
+import routerLoading from '../components/routerLoading';
 
 // router基路径
 const ROUTER_BASE = process.env.REACT_APP_ROUTER_BASE;
