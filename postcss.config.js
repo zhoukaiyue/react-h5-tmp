f5bda06e667a7deab1dc0cfcf585bf749800d0d9
zhoukaiyue <1301524439@qq.com>
2022-08-08 15:39:09 +0800

    fix: postcss

--- postcss.config.js
+++ postcss.config.js
@@ -4,11 +4,14 @@
  * @Author: zhoukai
  * @Date: 2022-07-29 10:05:44
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-02 16:10:42
+ * @LastEditTime: 2022-08-08 15:22:31
  */
 // postcss.config.js
 module.exports = {
     plugins: {
+        autoprefixer: {
+            overrideBrowserslist: ['Android 4.1', 'iOS 7.1', 'Chrome > 31', 'ff > 31', 'ie >= 8']
+        },
         // postcss-pxtorem 插件的版本需要 >= 5.0.0
         'postcss-pxtorem': {
             rootValue({ file }) {
