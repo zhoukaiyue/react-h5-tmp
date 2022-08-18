8de3c7659a518735b19a464263da0e124ff77376
zhoukaiyue <1301524439@qq.com>
2022-08-18 14:41:38 +0800

    fix: 兼容到ie11

--- src/index.js
+++ src/index.js
@@ -4,19 +4,20 @@
  * @Author: zhoukai
  * @Date: 2022-07-28 09:54:02
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-12 16:30:47
+ * @LastEditTime: 2022-08-18 14:30:09
  */
-/*
- * @Description: 描述
- */
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
-import '@/packages/console';
 import { createRoot } from 'react-dom/client';
 import '@/assets/css/index.scss';
 import App from '@/views/app/App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from '@/packages/store';
+// vconsole
+import '@/packages/console';
+
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
--- src/packages/console/index.js
+++ src/packages/console/index.js
@@ -1,13 +1,26 @@
+/* eslint-disable no-new */
 /*
  * @Descripttion:
  * @version:
  * @Author: zhoukai
  * @Date: 2022-08-12 16:10:17
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-12 16:33:06
+ * @LastEditTime: 2022-08-18 14:35:14
  */
-// 由于 vconsole 包比较大，仅在test环境下开启，生产环境请谨慎开启。
+
+/** TODO:由于 vconsole 包比较大，仅在test环境下开启，生产环境请谨慎开启。 */
+import fetch from 'isomorphic-fetch';
+import Proxy from 'es6-proxy-polyfill';
 import VConsole from 'vconsole';
+// /** 兼容ie浏览器vconsole插件 */
+if (!window.fetch) {
+    window.fetch = fetch;
+}
+if (!window.Proxy) {
+    window.Proxy = Proxy;
+}
+
+/** 开启vconsole */
 if (process.env.REACT_APP_MODE === 'test') {
     // 配置参考https://github.com/Tencent/vConsole/blob/HEAD/doc/tutorial.md
     // or init with options
