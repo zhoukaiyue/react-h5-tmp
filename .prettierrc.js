5fd2ce0e034bb236e806eff9988f4859e3b1ad35
zhoukaiyue <1301524439@qq.com>
2023-01-05 10:02:16 +0800

    style: 针对HTMl内联元素格式化皆采用「空格不敏感格式化」

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,7 +4,7 @@
  * @Author: zhoukai
  * @Date: 2022-08-01 21:28:57
  * @LastEditors: zhoukai
- * @LastEditTime: 2022-08-04 15:44:52
+ * @LastEditTime: 2023-01-05 09:58:07
  */
 module.exports = {
     semi: true,
@@ -12,5 +12,6 @@ module.exports = {
     printWidth: 120,
     singleQuote: true,
     jsxSingleQuote: true,
-    trailingComma: 'none'
+    trailingComma: 'none',
+    htmlWhitespaceSensitivity: 'ignore' //所有空格被视为不敏感，皆采用「空格不敏感格式化」
 };
